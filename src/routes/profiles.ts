ee01b26dd351eefb08cad37a36c5f2ae5b5a4b21
ThomasMlh <storm62320@gmail.com>
2022-05-24 08:48:13 +0200

    handle api error + prettier

--- src/routes/profiles.ts
+++ src/routes/profiles.ts
@@ -1,54 +1,54 @@
 import express, { Request, Response } from "express";
 import { Profile, validateProfile as validate } from "../models/profile";
 import { IProfile } from "src/types";
-
+import { handleApiError } from "src/utils/handleApiError";
 
 const router = express.Router();
 
 router.get("/", async (_: Request, res: Response) => {
-    try {
+  try {
+    const profiles = await Profile.find();
+    if (!profiles)
+      return res.status(404).send({ message: "Profiles not found" });
 
-        const profiles = await Profile.find();
-        if (!profiles) return res.status(404).send({ message: "Profiles not found" });
+    res.send(profiles);
 
-        res.send(profiles);
-
-        return true
-    } catch (error) {
-        return error
-    }
-})
+    return true;
+  } catch (error) {
+    handleApiError(error);
+    return false;
+  }
+});
 
 router.post("/", async (req: Request, res: Response) => {
-    try {
-        const { error } = validate(req.body)
-        console.log(error);
-        
-        if (error) return res.status(400).send({ message: "Bad request" });
-        const newProfile: IProfile = new Profile(req.body);
+  try {
+    const { error } = validate(req.body);
+    console.log(error);
 
-        await newProfile.save()
+    if (error) return res.status(400).send({ message: "Bad request" });
+    const newProfile: IProfile = new Profile(req.body);
 
-        res.send({ message: "Profile created" })
+    await newProfile.save();
 
-        return true
+    res.send({ message: "Profile created" });
 
-    } catch (error) {
-        return error
-    }
-})
+    return true;
+  } catch (error) {
+    handleApiError(error);
+    return false;
+  }
+});
 
 router.delete("/:id", async (req: Request, res: Response) => {
-    try {
-        const profile = await Profile.findByIdAndDelete(req.params.id);
-        if (!profile) return res.status(404).send({ message: "Profile not found" });
+  try {
+    const profile = await Profile.findByIdAndDelete(req.params.id);
+    if (!profile) return res.status(404).send({ message: "Profile not found" });
 
-        res.send({ message: "Profile deleted" })
-        return true
-    } catch (error) {
-        return error;
-    }
-})
-
+    res.send({ message: "Profile deleted" });
+    return true;
+  } catch (error) {
+    return error;
+  }
+});
 
 export default router;
